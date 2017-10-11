b37989e44c2de2686df7268ebe04fc5e006608a5
MartinBal <martinballot@gmail.com>
2017-10-11 19:19:43 +0200

    Modification des conditions de suppression

--- sc_suprEntités.py
+++ sc_suprEntités.py
@@ -1,5 +1,4 @@
 def delParcelles(layer):
     features=layer.getFeatures()
-    idsToDelete=[f.id() for f in features if (f['surf']<100 and f['p2_a']<75)]
+    idsToDelete=[f.id() for f in features if (f['surf']<75 or (f['surf']<150 and f['p2_a']<75))]
     layer.dataProvider().deleteFeatures(idsToDelete)
-    
