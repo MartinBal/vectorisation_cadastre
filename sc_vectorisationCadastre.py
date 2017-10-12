450b541fc5e8ec6498fc8d00f93b53447b7c5724
MartinBal <martinballot@gmail.com>
2017-10-12 17:47:36 +0200

    Ajout fonction vectorisation tampon

--- sc_vectorisationCadastre.py
+++ sc_vectorisationCadastre.py
@@ -12,10 +12,10 @@ def vectorisationCadastre(rasterCadastre, ref_projection='EPSG:3945' ):
     '''fonction permettant de polygoniser le cadastre raster'''
 
     #reprojection du cadastre en RGF93 - Lambert93 (EPSG : 2154)
-    ref_rasterCadastre=processing.runalg('gdalogr:warpreproject', rasterCadastre,ref_projection,'EPSG:2154',None,0.0,0,5,4,75.0,6.0,1.0,False,0,False,None,None)
+    ref_rasterCadastre_reproj=processing.runalg('gdalogr:warpreproject', rasterCadastre,ref_projection,'EPSG:2154',None,0.0,0,5,4,75.0,6.0,1.0,False,0,False,None,None)
 
     #### Polygonisation finne
-    ref_vecteurCadastre=processing.runalg('gdalogr:polygonize', ref_rasterCadastre['OUTPUT'],'DN',None)
+    ref_vecteurCadastre=processing.runalg('gdalogr:polygonize', ref_rasterCadastre_reproj['OUTPUT'],'DN',None)
     ref_vecteurCadastre=processing.runalg('qgis:extractbyattribute', ref_vecteurCadastre['OUTPUT'],'DN',0,'0',None)
 
     # Affinage
@@ -42,16 +42,12 @@ def vectorisationCadastre(rasterCadastre, ref_projection='EPSG:3945' ):
         if f['surf']>featCadre['surf']: featCadre=f
     vecteurCadastre.dataProvider().deleteFeatures([featCadre.id()])
 
-    #Comblement des parcelles avec les parcelles Tampon
 
     ####Extraction des parcelles tampon (polygonisation grossière)
-    # RasterBuffer 4m
-    # Invert data/noData
-    # Polygonisation
-    # Fill holes 200?
-    # simplification 0,25
-    # Calcul des indexs
-    #Suppression des artéfactes (s<75 ou s<100 et p2_a<75)
+    ref_vecteurCadastreTampon=vectorisationTampon(ref_rasterCadastre_reproj)
+
+    vecteurCadastreTampon = iface.addVectorLayer(ref_vecteurCadastreTampon['OUTPUT_LAYER'], "ParcellesCadastrales", "ogr")
+    netoyerParcelles(vecteurCadastreTampon)
 
     #Comblement du polygonne fin
     #Affinage
@@ -102,3 +98,20 @@ def comblerLacunes(vecteurCadastre, distBuff=0.75):
             vecteurCadastre.dataProvider().deleteFeatures([fid])
         else:
             vecteurCadastre.dataProvider().changeGeometryValues({ fid : tmp_geom })
+
+def vectorisationTampon(ref_rasterCadastre_reproj, distBuff=4, maxRing=250 ):
+    '''fonction permettant de polygoniser le cadastre raster'''
+
+    ref_rasterCadastre=processing.runalg('saga:gridbuffer', ref_rasterCadastre['OUTPUT'],distBuff,0,None)
+    ref_rasterCadastre=processing.runalg('saga:invertdatanodata', ref_rasterCadastre['BUFFER'],None)
+    ref_vecteurCadastre=processing.runalg('gdalogr:polygonize', ref_rasterCadastre['OUTPUT'],'DN',None)
+
+    ref_vecteurCadastre=processing.runalg('qgis:fillholes', ref_vecteurCadastre['OUTPUT'],maxRing,None)
+    ref_vecteurCadastre=processing.runalg('qgis:simplifygeometries', ref_vecteurCadastre['Results'],0.25,None)
+    ref_vecteurCadastre=processing.runalg('qgis:fieldcalculator', ref_vecteurCadastre['OUTPUT'],'surf',0,10.0,2.0,True,'$area',None)
+    ref_vecteurCadastre=processing.runalg('qgis:fieldcalculator', ref_vecteurCadastre['OUTPUT_LAYER'],'p2_a',0,25.0,2.0,True,'($perimeter)^2/surf',None)
+
+    return ref_vecteurCadastre
+    vecteurCadastreTampon = iface.addVectorLayer(ref_vecteurCadastre['OUTPUT_LAYER'], "ParcellesCadastrales", "ogr")
+
+    netoyerParcelles(vecteurCadastreTampon)
